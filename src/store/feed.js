dcd2a2989dbabe961246c4c01a1309c0ff7f52d3
prm3theus <null>
2020-05-23 16:30:06 -0400

    trying something out...

--- src/store/feed.js
+++ src/store/feed.js
@@ -1,11 +1,29 @@
 const hypercore = require('@geut/hypercore-promise')
 const ram = require('random-access-memory')
 const feed = hypercore(ram, {valueEncoding: 'json'})
-const replicate = require('@hyperswarm/replicator-web')
+const pump = require('pump')
+// todo
+// const replicate = require('@hyperswarm/replicator')
+// const replicate = require('@hyperswarm/replicator-web')
+
+const hyperswarm = require('hyperswarm-web')
+
+const swarm = hyperswarm({
+	wsProxy: 'ws://localhost:4977'
+})
 
 feed.on('ready', () => {
+	swarm.join(feed.discoveryKey,{
+		lookup: true,
+		announce: true
+	})
+	console.log(`Using discovery key: ${feed.discoveryKey.toString('hex')}`)
 	console.log(`Using key: ${feed.key.toString('hex')}`)
-	replicate(feed, { live: true, announce: true, wsProxy: 'ws://localhost:4977'})
+	// swarm = replicate(feed, { live: true, announce: true, wsProxy: 'ws://localhost:4977'})
+	swarm.on('connection', (socket, details) => {
+		// console.log(connection)
+		pump(socket, feed.replicate(true), socket)
+	})
 })
 
 // listen on append events
--- test/index.js
+++ test/index.js
@@ -1,16 +1,52 @@
-const ram = require('random-access-memory')
-const replicate = require('@hyperswarm/replicator-web')
 const hypercore = require('hypercore')
+const ram = require('random-access-memory')
+// const replicate = require('@hyperswarm/replicator-web')
+const hyperswarm = require('hyperswarm-web')
+const pump = require('pump')
 
-if(!process.argv[2]){
-	throw new Error('Please include a key.')
-}
-const key = Buffer.from(process.argv[2], 'hex')
-console.log(key)
-
+const IS_HOST = process.argv.length > 2 ? false : true
+const key = !IS_HOST && Buffer.from(process.argv[2], 'hex')
+console.log(`HOST: ${IS_HOST}`)
 const feed = key ? hypercore(ram, key, {valueEncoding: 'json'}) : hypercore(ram, {valueEncoding: 'json'})
 
-feed.on('ready', () => {
-	console.log(`Using Room: ${feed.key.toString('hex')}`)
-	replicate(feed, { live: true, wsProxy: 'ws://localhost:4977'})
+const swarm = hyperswarm({
+	wsProxy: 'ws://localhost:4977'
 })
+
+// const replicate = require('@hyperswarm/replicator-web')
+
+// feed.on('ready', () => {
+// 	replicate(feed, { announce: true, wsProxy: 'ws://localhost:4977' })
+// })
+
+feed.on('ready', () => {
+	console.log('ready')
+
+	swarm.join(feed.discoveryKey, {
+		lookup: true,
+		announce: true
+	})
+
+	console.log(`using key: ${feed.key.toString('hex')}`)
+	console.log(`using discovery key: ${feed.discoveryKey.toString('hex')}`)
+
+	// swarm = replicate(feed, { announce: true, wsProxy: 'ws://localhost:4977' })
+
+	swarm.on('connection', (socket, details) => {
+	  console.log('new connection!')
+
+		pump(socket, feed.replicate(IS_HOST), socket)
+	})
+
+	feed.createReadStream( { live: true } ).on('data', (data) => {
+		console.log('data')
+		console.log(data)
+	})
+	
+})
+
+let i=0
+
+IS_HOST && setInterval(() => feed.append({hi: IS_HOST + i++}), 1000)
+
+feed.createReadStream({ live: true }).on('data', (data) => console.log(data))
